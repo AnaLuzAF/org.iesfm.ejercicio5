6e26ca1456e14ee15b467f5d33d5d0d92adf86b4
analuz <analuzaviles@gmail.com>
2021-09-21 12:53:01 +0200

    Initial commit Ejercicio5

---
+++ src/main/java/org/iesfm/ejercicio5/Ejercicio5.java
@@ -0,0 +1,32 @@
+package org.iesfm.ejercicio5;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class Ejercicio5 {
+     private final static Logger log = LoggerFactory.getLogger(Ejercicio5.class);
+
+    private static void move(String origin, String destination) {
+        log.info("Moviendo el archivo " + origin + " a " + destination);
+        File file = new File(origin);
+        file.renameTo(new File(destination));
+    }
+
+    private static void delete(String path) {
+        log.info("Eliminando directorio " + path);
+        File file = new File(path);
+        file.delete();
+    }
+
+    // Cambia después el nombre de f11.txt por f12.txt, mueve el
+    // fichero f21.txt al directorio d1 y eliminando después el directorio d2.
+
+    public static void main(String[] args) {
+        move("/tmp/d1/f11.txt", "/tmp/d1/f12.txt");
+        move("/tmp/d2/f21.txt", "/tmp/d1/f21.txt");
+        delete("/tmp/d2");
+    }
+    }
+
